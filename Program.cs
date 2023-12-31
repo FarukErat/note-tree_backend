0e34af2fba2056553d5d87db07a24271b10eceab
Faruk ERAT <farukerat1327@gmail.com>
2023-12-31 17:54:49 +0300

    added TODOs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+// TODO: Make use of HttpContext.User
+// TODO: Re-design dependencies for better DDD
+// TODO: Use Redis Json to allow for query
+// TODO: Include a random property in session and use HMAC to produce CSRF token
+// TODO: Use strict CSRF token and lax session id
+
 var builder = WebApplication.CreateBuilder(args);
 var startup = new NoteTree.Startup(builder.Configuration);
 startup.ConfigureServices(builder.Services);
