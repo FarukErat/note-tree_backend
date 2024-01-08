fc2b157e0bb46809736493dc3f363d8a18a8c335
Faruk ERAT <farukerat1327@gmail.com>
2024-01-08 19:08:54 +0300

    took some notes for further development

--- Note/Models/Note.cs
+++ Note/Models/Note.cs
@@ -3,5 +3,5 @@ namespace NoteTree.Notes.Models;
 public sealed class Note
 {
     public string? Content { get; set; }
-    public List<Note>? SubNotes { get; set; }
+    public List<Note>? Children { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+// TODO: Consider using Dapper instead of EF Core
+// TODO: Consider using SQL for Note model
+// TODO: Consider removing Cipher service
 // TODO: Make use of HttpContext.User
 // TODO: Re-design dependencies for better DDD
 // TODO: Use Redis Json to allow for query
 // TODO: Include a random property in session and use HMAC to produce CSRF token
-// TODO: Use strict CSRF token and lax session id
+// TODO: session id: lax, HttpOnly and secure
+// TODO: CSRF token: strict, not HttpOnly(as it will be sent via payload not header) and secure
 
 var builder = WebApplication.CreateBuilder(args);
 var startup = new NoteTree.Startup(builder.Configuration);
