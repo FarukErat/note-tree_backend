33277d8cf5d545ee467a28f6e7a45a6811bbb72d
Faruk ERAT <farukerat1327@gmail.com>
2024-01-28 16:16:38 +0300

    checked some todos

--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
 // TODO: Consider using Dapper instead of EF Core
 // TODO: Consider using SQL for Note model
 // TODO: Consider removing Cipher service
-// TODO: Make use of HttpContext.User
 // TODO: Re-design dependencies for better DDD
 // TODO: Use Redis Json to allow for query
-// TODO: Include a random property in session and use HMAC to produce CSRF token
 // TODO: session id: lax, HttpOnly and secure
 // TODO: CSRF token: strict, not HttpOnly(as it will be sent via payload not header) and secure
 
